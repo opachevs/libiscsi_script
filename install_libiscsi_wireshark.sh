cd355f639a57e701c4c2f859f0a43c6e8634f758
opachevs <opachevs@post.bgu.ac.il>
2016-05-29 09:16:32 +0300

    Add files via upload

---
+++ install_libiscsi_wireshark.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#install dependencies
+yum install -y unzip CUnit CUnit-devel make automake autoconf libtool wireshark wireshark-gnome xauth
+
+if [ -f master.zip ] ;then
+	rm master.zip
+fi
+
+if [ -d libiscsi-master ] ;then
+	rm -r libiscsi-master
+fi
+
+
+#download and extract package
+echo "Starting Installer"
+wget https://github.com/sahlberg/libiscsi/archive/master.zip
+unzip master.zip
+
+#compile
+cd libiscsi-master
+chmod 777 autogen.sh
+./autogen.sh
+./configure > /tmp/configure.results
+grep libcunit /tmp/configure.results |grep yes
+if [ $? -eq 0 ]; then
+	echo " ./configure run sucessfully"
+else
+	echo "libcunit error, run configure manually to check"
+	exit 1
+fi
+make
+./test-tool/iscsi-test-cu -l
+if [ $? -eq 0 ];then
+	echo "libiscsi installed sucessfully"
+else
+	echo "unexpected error"
+	exit 1
+fi
+
+
+
